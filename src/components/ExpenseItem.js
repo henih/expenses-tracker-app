4d0604a71290a13e342df4501e8be64502f773b6
henih <86572062+henih@users.noreply.github.com>
2023-09-04 18:26:46 +0200

    Initial Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>4th September 2023</div>
+      <div>
+        <h2>Insurance</h2>
+      </div>
+      <div>CHF 300</div>
+    </div>
+  );
+}
