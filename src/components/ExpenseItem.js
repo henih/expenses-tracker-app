fb3bf1d28fe25dee352f1fde7f4b533487602f42
henih <86572062+henih@users.noreply.github.com>
2023-09-05 09:59:23 +0200

    Initial CSS styling added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>4th September 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Insurance</h2>
       </div>
-      <div>CHF 300</div>
+      <div className="expense-item__price ">CHF 300</div>
     </div>
   );
 }
